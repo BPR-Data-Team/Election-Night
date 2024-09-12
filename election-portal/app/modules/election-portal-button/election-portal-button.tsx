9d16d18ec098d91769f9080c62bce519c8168712
Akshay <akshay5515aa@gmail.com>
2024-09-11 22:41:07 -0400

    Finished adding alt text as props and linking home page to all pages

--- election-portal/app/modules/election-portal-button/election-portal-button.tsx
+++ election-portal/app/modules/election-portal-button/election-portal-button.tsx
@@ -1,26 +1,31 @@
 import styles from "../election-portal-button/election-portal-button.module.css";
 import Image, { StaticImageData } from "next/image";
+import { useRouter } from "next/navigation";
 
 interface ElectionPortalButtonProps {
   title: String;
   image_location: StaticImageData;
   button_link: string;
+  alt_text: string;
 }
 export default function Election_Portal_Button(
   props: ElectionPortalButtonProps
 ) {
+  const router = useRouter();
   return (
-    <div className={styles.button_container}>
-      <div className={styles.button_col}>
-        <div className={styles.button_title}>{props.title}</div>
-        <div className={styles.button_image_container}>
-          <Image
-            className={styles.button_image}
-            src={props.image_location}
-            alt="This is a picture of a finance graph for aesthetic purposes"
-          ></Image>
+    <button onClick={() => router.push(props.button_link)}>
+      <div className={styles.button_container}>
+        <div className={styles.button_col}>
+          <div className={styles.button_title}>{props.title}</div>
+          <div className={styles.button_image_container}>
+            <Image
+              className={styles.button_image}
+              src={props.image_location}
+              alt={props.alt_text}
+            ></Image>
+          </div>
         </div>
       </div>
-    </div>
+    </button>
   );
 }
--- election-portal/app/page.tsx
+++ election-portal/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Election_Portal_Button from "./modules/election-portal-button/election-portal-button";
@@ -5,24 +6,28 @@ import finance_image from "./assets/finance.png";
 import trophy_image from "./assets/trophy.png";
 import how_to_vote_image from "./assets/how_to_vote.png";
 
+//TODO: Banner component
 export default function Home() {
   return (
     <div className={styles.page}>
       <div className={styles.row}>
         <Election_Portal_Button
           title={"Election Breakdown"}
-          button_link="/presidential-breakdown"
+          button_link="/election-breakdown"
           image_location={finance_image}
+          alt_text="This is a picture of a finance graph for aesthetic purposes"
         ></Election_Portal_Button>
         <Election_Portal_Button
           title={"Road to Control"}
           button_link="/road-to-control"
           image_location={trophy_image}
+          alt_text="This is a picture of a trophy"
         ></Election_Portal_Button>
         <Election_Portal_Button
           title={"Exit Poll Explorer"}
           button_link="/exit-poll-explorer"
           image_location={how_to_vote_image}
+          alt_text="This is a picture of a ballot box aesthetic purposes"
         ></Election_Portal_Button>
       </div>
     </div>
