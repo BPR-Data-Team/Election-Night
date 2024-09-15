1660017d62040187d86e3d352c0014d37a0cdef8
Akshay Mehta <36643800+akslaym@users.noreply.github.com>
2024-09-15 12:04:51 -0400

    Created an election breakdown page

--- election-portal/app/election-breakdown/page.tsx
+++ election-portal/app/election-breakdown/page.tsx
@@ -1,3 +1,63 @@
+import React, { useState } from "react";
+
+import Head from "next/head";
+import Image from "next/image";
+
+import styles from "./page.module.css";
+import Rightbar from "./components/rightbar";
+import Banner from "./components/banner";
+import Canvas from "./components/canvas";
+
 export default function Election_Breakdown_Page() {
-  return <p>Election Breakdown Page</p>;
+  const [isBannerVisible, setIsBannerVisible] = useState<boolean>(true);
+  const [drawMode, setDrawMode] = useState<boolean>(false);
+
+  const toggleBanner = () => {
+    setIsBannerVisible(!isBannerVisible);
+  };
+
+  const stringTest = (test: string) => {
+    console.log(test);
+  };
+
+  // If toggleDraw is true then we need to block mouse input to everything except Rightbar
+  const toggleDraw = () => {
+    setDrawMode(!drawMode);
+  };
+
+  return (
+    <>
+      <div className={styles.page}>
+        <div className="overflow-catch">
+          {/* Left title banner */}
+          <Banner
+            align="left"
+            height={6}
+            wordmark="24cast.org"
+            header=""
+            message="This is a top banner!"
+            isVisible={isBannerVisible}
+            toggleVisibility={toggleBanner}
+          />
+
+          {drawMode && <Canvas />}
+
+          {/* Needs to be topmost during content screens */}
+          <Rightbar
+            pageSwitch={toggleBanner}
+            exit={toggleBanner}
+            toggleDraw={toggleDraw}
+            availableBreakdowns={["asdf"]}
+            breakdownSwitch={stringTest}
+            availableYears={["1"]}
+            yearSwitch={stringTest}
+            isVisible={isBannerVisible}
+            toggleVisibility={toggleBanner}
+          />
+
+          {/* Future homepage topmost element */}
+        </div>
+      </div>
+    </>
+  );
 }
