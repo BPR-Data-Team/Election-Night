73013fdb09ec43a49cec0662e49f534e47faef32
Louis Geer <louisjgeer@gmail.com>
2024-10-01 15:48:53 -0400

    Fix for firefox

--- election-portal/app/modules/canvas/canvas.tsx
+++ election-portal/app/modules/canvas/canvas.tsx
@@ -1,16 +1,28 @@
-import { ReactSketchCanvas } from "react-sketch-canvas";
+import { ReactSketchCanvas, ReactSketchCanvasRef, CanvasRef } from "react-sketch-canvas";
 import './canvas.css';
+import { useEffect, useRef } from "react";
 
 export default function Canvas() {
+
+  // Fix for Firefox browser
+  // (https://github.com/replicate/scribble-diffusion/issues/31#issuecomment-1451288647)
+  const canvasDivRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const canvasMask = canvasDivRef.current?.querySelector<SVGGElement>('#react-sketch-canvas__stroke-group-0');
+
+    if (canvasMask) {
+      canvasMask.removeAttribute("mask");
+    }
+  }, []);
+
   return (
-    <div className="drawCanvas">
+    <div className="drawCanvas" ref={canvasDivRef}>
       <ReactSketchCanvas
         className="canvasComponent"
         width="100%"
         height="100%"
         canvasColor="transparent"
         strokeColor="#88EE88"
-        withViewBox
       />
     </div>
   );
