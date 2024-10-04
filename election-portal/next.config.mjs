d11f486a01e1c46ca4412f0fc96d81212e8cdd88
Ariel Shifrin <40341323+arielshif@users.noreply.github.com>
2024-10-04 09:25:36 -0400

    Update next.config.mjs

--- election-portal/next.config.mjs
+++ election-portal/next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    output: 'export', // Ensures Next.js exports static HTML
-  };
-  
-  export default nextConfig;
+  eslint: {
+    ignoreDuringBuilds: true, 
+  },
+  output: 'export', 
+  images: {
+    unoptimized: true, 
+  },
+  basePath: isProd ? "/Election-Night" : "", 
+  assetPrefix: isProd ? "/Election-Night/" : "", 
+};
+
+export default nextConfig;
