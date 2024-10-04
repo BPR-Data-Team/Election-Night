73fcc3c4004007144f2ad6f471f1fc2e3e45a400
Ariel Shifrin <40341323+arielshif@users.noreply.github.com>
2024-10-04 09:16:20 -0400

    Update next.config.mjs

--- election-portal/next.config.mjs
+++ election-portal/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  };
+  
+  export default nextConfig;
