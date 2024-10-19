ce0e2103d8c7d76d98099a2b6666cbdfce73e508
Equite774 <chai.harsha7@gmail.com>
2024-10-19 15:27:47 -0400

    updated sidebar controls

--- decision_desk/Dashboard/Dashboard Utilities/BettingOdds.R
+++ decision_desk/Dashboard/Dashboard Utilities/BettingOdds.R
@@ -1,7 +1,7 @@
 # This replicates the function of the earlier BettingsOdds.R implementation but modularizes it
 
 get_betting_odds <- function(election_type, selected_state) {
-  election_urls <- read_csv("../../cleaned_data/betting_odds_links.csv")
+  election_urls <- read_csv("../../cleaned_data/Locally-Hosted Data/betting_odds_links.csv")
   
   url_entry <- election_urls %>%
     filter(state == selected_state) %>%
--- decision_desk/Dashboard/Dashboard.R
+++ decision_desk/Dashboard/Dashboard.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinyWidgets)
 library(bslib)
 library(ggplot2)
 library(leaflet)
@@ -34,7 +35,10 @@ county_names <- read.csv("cleaned_data/FIPS References/county_fips.csv")
 electoral_votes <- read.csv("cleaned_data/ElectoralVotes.csv")
 all_races <- read_csv("cleaned_data/DDHQ_current_race_results.csv")
 election_types <- all_races %>% pull("office_type") %>% unique() %>% append(., "All", after = 0)
-
+poll_closing <- read.csv("cleaned_data/Locally-Hosted Data/poll_closing.csv")
+print(names(poll_closing))
+closing_times <- poll_closing %>% pull("Poll.Closing") %>% unique() %>% head(-1)
+print(closing_times)
 
 # ----------------------------- APP LOGIC ----------------------------------- #
 graphServer <- function(input, output, session) {
@@ -123,6 +127,30 @@ graphServer <- function(input, output, session) {
                         selected = "All"
       )
     })
+    # Update state dropdown based on the election type
+    observeEvent(input$category_select, {
+      # Filter states based on the selected election type
+      filtered_states <- all_races %>%
+        filter(office_type == input$category_select | input$category_select == "All") %>%
+        pull("state") %>%
+        unique() %>%
+        append("All", after = 0)
+      
+      # Check if the current state is in the filtered states
+      current_state <- input$state_select
+      if (current_state %in% filtered_states) {
+        selected_state <- current_state  # Keep the current state if it exists in the dropdown
+      } else {
+        selected_state <- "All"  # Fallback to "All" if the current state is not in the filtered states
+      }
+      # Update the state dropdown with the filtered states and maintain or reset the selection
+      updateSelectInput(session, "state_select",
+                        choices = filtered_states,
+                        selected = selected_state
+      )
+    })
+    output$selected_time <- renderText({closing_times[input$time_slider]})
+    
 }
 
 # Graph Module UI
@@ -142,24 +170,27 @@ graphOutputUI <- page_sidebar(
       choices = NULL,
       selected = NULL
     ),
-    selectInput(
-      inputId = "district_select",
-      label = "District:",
-      choices = NULL,
-      selected = NULL
+    sliderInput( 
+      inputId = "percent_reporting_bounds", 
+      label = "% Reporting:", 
+      min = 0, max = 100, 
+      value = c(30, 70),
+      step = 10
     ),
-    selectInput(
-      inputId = "lower_bound",
-      label = "Lower Bound (% Reporting):",
-      choices = seq(0, 100, by = 10),
-      selected = 0
+    sliderTextInput(
+      inputId = "poll_closing_bounds",
+      label = "Poll Closing Times:",
+      choices = closing_times,
+      selected = c(closing_times[1], closing_times[length(closing_times)]),
+      grid = TRUE,
+      force_edges = TRUE
     ),
-    selectInput(
-      inputId = "upper_bound",
-      label = "Upper Bound (% Reporting):",
-      choices = seq(10, 100, by = 10),  # Initially set to the full range
-      selected = 100
-    )  ),
+    checkboxInput(
+      inputId = "key_races",
+      label = "Key Races Only",
+      value = FALSE
+    )
+  ),
   fluidPage(
     layout_columns(
       fill = FALSE,
