480fad2cded9b99e1dad8949e267a7b61ec7cc66
Stella Tsogtjargal <stella_tsogtjargal@brown.edu>
2024-10-27 21:27:41 -0400

    exit poll data table

--- election-portal/types/Demographic.ts
+++ election-portal/types/Demographic.ts
@@ -1,9 +1,9 @@
 export enum Demographic {
-  All = "All",
   Age = "Age",
   Race = "Race",
   Gender = "Gender",
   Education = "Education",
   Income = "Income",
   AreaType = "Area type",
+  Region = "Region",
 }
