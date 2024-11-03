66eb2b403238a948434272b77bc101b3cebd6d02
Ariel Shifrin <40341323+arielshif@users.noreply.github.com>
2024-11-03 15:31:43 -0500

    Revert "Revert "Exit poll maps""

--- election-portal/app/election-breakdown/page.tsx
+++ election-portal/app/election-breakdown/page.tsx
@@ -53,7 +53,6 @@ export default function Election_Breakdown_Page() {
   // State Mode While National
   const SMWN = sharedState.view != State.National && displayNational;
 
-  // Would appreciate a better way to do this, if possible
   useEffect(() => {
     const wrapperDiv = document?.getElementById('mapWrapper');
     const EBMapDiv = document?.getElementById('EBContainer');
@@ -168,7 +167,6 @@ export default function Election_Breakdown_Page() {
           console.error('Error loading historical county data:', error)
         );
     }
-    // }
   }, []);
 
   if (!historicalElectionsData || !historicalCountyData)
